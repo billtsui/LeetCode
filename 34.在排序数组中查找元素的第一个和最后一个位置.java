11afcc38fc2d88af3c08dbc33509e7dfe39e01bf
Bill Tsui <billkuentsui@outlook.com>
2021-03-02 17:01:58 +0800

    刷二分查找的题目

---
+++ 34.在排序数组中查找元素的第一个和最后一个位置.java
@@ -0,0 +1,107 @@
+/*
+ * @lc app=leetcode.cn id=34 lang=java
+ *
+ * [34] 在排序数组中查找元素的第一个和最后一个位置
+ *
+ * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (42.29%)
+ * Likes:    871
+ * Dislikes: 0
+ * Total Accepted:    213.6K
+ * Total Submissions: 505.1K
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+ * 
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 
+ * 进阶：
+ * 
+ * 
+ * 你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+ * 
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 0 
+ * -10^9 
+ * nums 是一个非递减数组
+ * -10^9 
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = left_bound(nums, target);
+        result[1] = right_bound(nums, target);
+        return result;
+    }
+
+    private static int left_bound(int[] nums, int target){
+        int left = 0,right = nums.length - 1;
+        while (left <= right){
+            int middle = left + (right - left)/2;
+            if(nums[middle] == target){
+                right = middle - 1;
+            }else if(nums[middle] > target){
+                right = middle - 1;
+            }else if(nums[middle] < target){
+                left = middle + 1;
+            }
+        }
+
+        if(left >= nums.length || nums[left] != target){
+            return -1;
+        }
+        return left;
+    }
+
+    private static int right_bound(int[] nums,int target){
+        int left = 0,right = nums.length - 1;
+        while (left <= right){
+            int middle = left + (right - left)/2;
+            if(nums[middle] == target){
+                left = middle + 1;
+            }else if(nums[middle] > target){
+                right = middle - 1;
+            }else if(nums[middle] < target){
+                left = middle + 1;
+            }
+        }
+
+        if(right < 0 || nums[right] != target){
+            return -1;
+        }
+        return right;
+    }
+}
+// @lc code=end
+
---
+++ 69.x-的平方根.java
@@ -0,0 +1,56 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=java
+ *
+ * [69] x 的平方根
+ *
+ * https://leetcode-cn.com/problems/sqrtx/description/
+ *
+ * algorithms
+ * Easy (39.22%)
+ * Likes:    606
+ * Dislikes: 0
+ * Total Accepted:    263.6K
+ * Total Submissions: 672K
+ * Testcase Example:  '4'
+ *
+ * 实现 int sqrt(int x) 函数。
+ * 
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ * 
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ * 
+ * 示例 1:
+ * 
+ * 输入: 4
+ * 输出: 2
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842..., 
+ * 由于返回类型是整数，小数部分将被舍去。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+        int left = 0, right = x;
+		int ans = -1;
+		while (left <= right){
+			int middle = left + (right - left) / 2;
+			if((long)middle * middle <= x){
+				ans =  middle;
+				left = middle + 1;
+			}else{
+				right = middle - 1;
+			}
+		}
+		return ans;
+    }
+}
+// @lc code=end
+
